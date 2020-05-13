b9f6d4ff28b33cc4fa86b9f2d5f0aa4895519908
发粪涂墙 <569617079@qq.com>
2020-05-13 11:18:14 +0800

    rename js/global_广告过滤，纯净助手.js to js/global_广告过滤，纯净上网.js.

---
+++ js/global_广告过滤，纯净上网.js
@@ -0,0 +1,83 @@
+// ==UserScript==
+// @name         广告过滤，纯净上网
+// @namespace    sg_dog_document
+// @version      1.0.1
+// @description  好网站，要共享
+// @author       xxoo
+// @include      *://0xxxkk.com/*
+// @include      *://xxxkk0.com/*
+// @include      *://1xxxkk.com/*
+// @include      *://xxxkk1.com/*
+// @include      *://2xxxkk.com/*
+// @include      *://xxxkk2.com/*
+// @include      *://3xxxkk.com/*
+// @include      *://xxxkk3.com/*
+// @include      *://4xxxkk.com/*
+// @include      *://xxxkk4.com/*
+// @include      *://5xxxkk.com/*
+// @include      *://xxxkk5.com/*
+// @include      *://6xxxkk.com/*
+// @include      *://xxxkk6.com/*
+// @include      *://7xxxkk.com/*
+// @include      *://xxxkk7.com/*
+// @include      *://8xxxkk.com/*
+// @include      *://xxxkk8.com/*
+// @include      *://9xxxkk.com/*
+// @include      *://xxxkk9.com/*
+// @include      *://0xxxnn.com/*
+// @include      *://xxxnn0.com/*
+// @include      *://1xxxnn.com/*
+// @include      *://xxxnn1.com/*
+// @include      *://2xxxnn.com/*
+// @include      *://xxxnn2.com/*
+// @include      *://3xxxnn.com/*
+// @include      *://xxxnn3.com/*
+// @include      *://4xxxnn.com/*
+// @include      *://xxxnn4.com/*
+// @include      *://5xxxnn.com/*
+// @include      *://xxxnn5.com/*
+// @include      *://6xxxnn.com/*
+// @include      *://xxxnn6.com/*
+// @include      *://7xxxnn.com/*
+// @include      *://xxxnn7.com/*
+// @include      *://8xxxnn.com/*
+// @include      *://xxxnn8.com/*
+// @include      *://9xxxnn.com/*
+// @include      *://xxxnn9.com/*
+// @include      *://0xxxaa.com/*
+// @include      *://xxxaa0.com/*
+// @include      *://1xxxaa.com/*
+// @include      *://xxxaa1.com/*
+// @include      *://2xxxaa.com/*
+// @include      *://xxxaa2.com/*
+// @include      *://3xxxaa.com/*
+// @include      *://xxxaa3.com/*
+// @include      *://4xxxaa.com/*
+// @include      *://xxxaa4.com/*
+// @include      *://5xxxaa.com/*
+// @include      *://xxxaa5.com/*
+// @include      *://6xxxaa.com/*
+// @include      *://xxxaa6.com/*
+// @include      *://7xxxaa.com/*
+// @include      *://xxxaa7.com/*
+// @include      *://8xxxaa.com/*
+// @include      *://xxxaa8.com/*
+// @run-at       document-end
+// ==/UserScript==
+
+(function(){
+  'use strict';
+  var de = document.getElementById("content");
+  if (de) {
+  for(var i=0; i<de.childElementCount; i++){
+    var x=de.children[i];
+    if (["body","header"].indexOf(x.id)==-1) x.hidden=true;
+  }}else{
+    var aa = document.getElementsByTagName('h2')[2];
+    var ss = aa.innerText;
+    var bb = document.getElementsByTagName('input')[0];
+    bb.value = ss;
+    var cc = document.getElementsByTagName('button')[0];
+    cc.click();
+  }
+})();
